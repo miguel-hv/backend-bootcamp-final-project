17d187882d0a0dadaeaf1fc2edd44dda65d59376
@Loretodpinedo <ororomb@yahoo.es>
2021-05-18 12:50:25 +0200

    Server clean, ready to start

--- routes/index.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const indexController = require('../controllers/index.controller');
-
-const router = express.Router();
-
-router.get('/', indexController.indexGet);
-
-module.exports = router;
