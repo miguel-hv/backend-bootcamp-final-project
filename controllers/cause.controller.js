65726c8c4827f598a0d46de626ed31a894b8e80b
mhdelavega <huertadelavegamiguel@gmail.com>
2021-05-23 11:41:32 +0200

    FEAT: user model changes

--- controllers/cause.controller.js
+++ controllers/cause.controller.js
@@ -1,19 +1,41 @@
 const passport = require("passport");
 const Cause = require('../models/Cause.model');
+const User = require('../models/User.model');
 
 
 module.exports = {
+
+    causesGet: async (req, res, next) => {
+
+        try {
+            const causes = await Cause.find();
+            
+            return res.json(causes);
+        } catch(error) {
+            console.log('error causesGet', error);
+            return next(error);
+        }
+    },
+
     causeDonationPost: async (req, res, next) => {
         try {
 
             // const { donation, cause } = req.body;
             console.log(req.body);
-            let { donation } = req.body;
+            let { cause, donation } = req.body;
             console.log('donation recibida en controller:'+donation);
-
-            await Cause.updateOne({ name: "Incendios en Australia" },{ $inc: { raised: donation }}); 
-        
+            console.log('cause recibida en controller:'+cause);
             
+            await Cause.updateOne({ name: cause},{ $inc: { raised: donation }}); 
+            // causeId = await Cause.findAndModify({query: { name: cause }, update: { $inc: { raised: donation }}}); 
+            // causeId = Cause.find({ name: cause }, { id_:1 }); 
+            // user = "musico@upgrade.com";
+            // await User.updateOne({ email: user },{ causeId });
+
+            // usuario = await User.findAndModify({
+            //     query: { name: cause }, 
+            //     update:{ $push: { donations: { causeId, donation }}}
+            // });
     
             return res.status(201).json('causeDonation completed');
     
--- routes/cause.routes.js
+++ routes/cause.routes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const controller = require('../controllers/cause.controller');
 const router = express.Router();
 
+router.get('/', controller.causesGet);
 router.post('/donation', controller.causeDonationPost);
 
 module.exports = router;
